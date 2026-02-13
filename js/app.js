27a9d6e1447966827df723c58af167229cd20cb7
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-02-13 08:50:33 -0300

    CREATE: App Shopsmart

---
+++ js/app.js
@@ -0,0 +1,133 @@
+
+const input1 = document.getElementById('valor01');
+const input2 = document.getElementById('valor02');
+
+const resultado = document.getElementById('resultado');
+
+
+function calcularTotal(){
+    const v1 = parseFloat(input1.value);
+    const v2 = parseFloat(input2.value);
+
+    if (isNaN(v1) || isNaN(v2)){
+        alert('Os campos não foram preenchidos corretamente. Digite um número.')
+    
+    } else{
+        resultado.style.display = 'flex';
+        resultado.innerHTML = 
+        `          
+    <ul>
+            <li>O preço do item desejado é R$<span> ${v1} </span> </li>
+            <li>A quantidade de itens é: <span>${v2}</span> </li>       
+            <li>O total a pagar é: R$<span>${v1 * v2}</span> </li>
+    </ul>
+        `;
+    
+        
+    }
+
+}
+
+function desconto(){
+    const v1 = parseFloat(input1.value);
+    const v2 = parseFloat(input2.value);
+    let desconto = (v1*v2)/100;
+
+    if (isNaN(v1) || isNaN(v2)){
+        alert('Os campos não foram preenchidos corretamente. Digite um número.')
+    
+    } else{
+        resultado.style.display = 'flex';
+        resultado.innerHTML = 
+        `          
+    <ul>
+            <li>O preço do item desejado é R$<span> ${v1} </span> </li>
+            <li>O desconto aplicado é de: <span>${v2}</span>% </li>       
+            <li>O total a pagar é: R$<span>${v1 - desconto}</span> </li>
+    </ul>
+        `;
+    
+        
+    }
+
+}
+
+function juros() {
+    const v1 = parseFloat(input1.value);
+    const v2 = parseFloat(input2.value);
+    let acrescimo = (v1*v2)/100;
+
+    if (isNaN(v1) || isNaN(v2)){
+        alert('Os campos não foram preenchidos corretamente. Digite um número.')
+    
+    } else{
+        resultado.style.display = 'flex';
+        resultado.innerHTML = 
+        `          
+    <ul>
+            <li>O preço do item é R$<span> ${v1} </span> </li>
+            <li>Os juros são de <span>${v2}</span>% </li>       
+            <li>O total a pagar é: R$<span>${acrescimo + v1}</span> </li>
+    </ul>
+        `;
+    
+        
+    }
+
+}
+
+function comissao(){
+    const v1 = parseFloat(input1.value);
+    const v2 = parseFloat(input2.value);
+    const taxa = v1*(v2/100);
+
+    if (isNaN(v1) || isNaN(v2)){
+        alert('Os campos não foram preenchidos corretamente. Digite um número.')
+    
+    } else{
+        resultado.style.display = 'flex';
+        resultado.innerHTML = 
+        `          
+    <ul>
+            <li>O preço do item desejado é R$ <span> ${v1} </span> </li>
+            <li>A taxa aplicada é: <span>${v2}</span>% </li>       
+            <li>A comissão é de R$<span>${taxa}</span> </li>
+    </ul>
+        `;
+    
+        
+    }
+
+}
+
+function lucro(){
+    const v1 = parseFloat(input1.value);
+    const v2 = parseFloat(input2.value);
+    const lucro = v1 - v2;
+
+    if (isNaN(v1) || isNaN(v2)){
+        alert('Os campos não foram preenchidos corretamente. Digite um número.')
+    
+    } else{
+        resultado.style.display = 'flex';
+        resultado.innerHTML = 
+        `          
+    <ul>
+            <li>O preço de venda é R$<span> ${v1} </span> </li>
+            <li>O custo foi de R$<span>${v2}</span> </li>       
+            <li>O lucro total é de R$<span>${lucro}</span> </li>
+    </ul>
+        `;
+    
+        
+    }
+
+}
+
+function limpar(){
+    resultado.style.display = 'none';
+    input1.value = '';
+    input2.value = '';
+    input1.focus();
+
+}
